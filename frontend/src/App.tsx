033e950d381676655bc66e746f7b5a191b15ab34
corban <corbanpro@gmail.com>
2024-10-07 17:59:14 -0600

    add react frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
